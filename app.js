0aff9b47762a175926dfc73c46fdf7229dd0370e
Amir Afshar <mr.afshar0@gmail.com>
2021-04-27 01:23:10 +0430

    Add the battle log section logic and styles

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const game = Vue.createApp({
       monsterHealth: 100,
       winner: null,
       currentRound: 0,
+      logMessages: [],
     };
   },
   watch: {
@@ -29,6 +30,7 @@ const game = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      this.logMessages = [];
     },
     surrender() {
       this.winner = "monster";
@@ -38,22 +40,33 @@ const game = Vue.createApp({
       this.monsterHealth -= attackValue;
       this.monsterAttack();
       this.currentRound++;
+      this.addLogMessage("player", "attack", attackValue);
     },
     monsterAttack() {
       const attackValue = randomValue(10, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
     },
     specialAttack() {
       const attackValue = randomValue(15, 25);
       this.monsterHealth -= attackValue;
       this.monsterAttack();
       this.currentRound++;
+      this.addLogMessage("player", "attack", attackValue);
     },
     playerHeal() {
       const healValue = randomValue(8, 20);
       this.playerHealth += healValue;
       this.monsterAttack();
       this.currentRound++;
+      this.addLogMessage("player", "heal", healValue);
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
   computed: {
